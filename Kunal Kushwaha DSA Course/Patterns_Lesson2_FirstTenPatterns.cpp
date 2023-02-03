1db5765cff32cdce018c7034f8bda5c182ef4812
princepatrick <princepatrickanand1996@gmail.com>
2023-02-03 12:45:38 -0500

    Kunal Kushwaha DSA - Patterns - Pattern 2
    Code for pattern2 from Patterns in Kunal's Course in DSA

--- Kunal Kushwaha DSA Course/Patterns_Lesson2_FirstTenPatterns.cpp
+++ Kunal Kushwaha DSA Course/Patterns_Lesson2_FirstTenPatterns.cpp
@@ -11,9 +11,26 @@ void pattern1(int n){
 
 }
 
+void pattern2( int n ){
+    
+    for( int i=0 ; i<n; i++ ){
+        for( int j=0 ; j<n ; j++ ){
+            if( j<=i ){
+                std::cout<<"*";
+            }
+        }
+        std::cout<<std::endl;
+    }
+}
+
 int main(){
     int n;
     std::cout<<"Enter the size for which you want the pattern to be printed"<<std::endl;
     std::cin>>n;
+
+    std::cout<<"Pattern 1"<<std::endl;
     pattern1(n);
+
+    std::cout<<"Pattern 2"<<std::endl;
+    pattern2(n);
 }
